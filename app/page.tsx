b103d3511e2f771659fa42a68e5b17725eb26552
Ayden Xu <aydenxunc@gmail.com>
2025-01-19 15:09:56 -0500

    lol

--- app/page.tsx
+++ app/page.tsx
@@ -86,7 +86,7 @@ const MainContent = () => {
 
   const handleAffiliateClick = async () => {
     await new Promise((resolve) => setTimeout(resolve, 800));
-    window.location.href = "https://glstrck.com/aff_c?offer_id=474&aff_id=11848";
+    window.location.href = "https://glstrck.com/aff_c?offer_id=446&aff_id=11848";
   };
 
   return (
