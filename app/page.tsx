3384c83dfb13f3c82513c054b33b8daa5536b092
Ayden Xu <aydenxunc@gmail.com>
2025-04-06 19:01:35 -0400

    sjfi3d

--- app/page.tsx
+++ app/page.tsx
@@ -86,7 +86,7 @@ const MainContent = () => {
 
   const handleAffiliateClick = async () => {
     await new Promise((resolve) => setTimeout(resolve, 800));
-    window.location.href = "https://glstrck.com/aff_c?offer_id=1999&aff_id=11848";
+    window.location.href = "https://glstrck.com/aff_c?offer_id=712&aff_id=11848";
   };
 
   return (
